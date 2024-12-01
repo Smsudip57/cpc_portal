1a7d0907571e2aa64f151693a29f35bf8e035458
Sudip Mandal <sudip7981@gmail.com>
2024-12-02 00:46:35 +0600

    sd

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,7 +4,7 @@ import Navbar from '@/components/navbar'
 import AdminSideBar from '@/components/adminSidebar'
 import { useRouter } from 'next/navigation'
 
-export default function page() {
+export default function Page() {
   const router = useRouter()
   router.push('/admin/publishnews')
   return (
