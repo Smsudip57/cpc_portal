1e249056a11aa85841678ffd9c972e29504a7b56
Sudip Mandal <sudip7981@gmail.com>
2024-12-03 08:50:23 +0600

    df

--- app/api/events/registeredevent/route.js
+++ app/api/events/registeredevent/route.js
@@ -1,4 +1,4 @@
-import dbConnect from '@/connect/dbConnect'; // Your DB connection utility
+import dbConnect from '@/connect/dbConnect';
 import Event from '@/models/event';
 import { NextResponse } from 'next/server';
 import jwt from 'jsonwebtoken';
@@ -6,7 +6,9 @@ import User from '@/models/user';
 
 export async function GET(req) {
   try {
+
     await dbConnect();
+
     const cookie = req.cookies.get('user')?.value || '';
     const token = cookie.split(';')[0].trim();
 
@@ -35,41 +37,17 @@ export async function GET(req) {
       );
     }
 
-    // Retrieve ongoing events
     const events = await Event.find({
-        end: { $gte: new Date() }, // Fetch events that haven't ended yet
+        end: { $gte: new Date() }, 
       });
       
-    // Filter events where the user is a participant
-    // const userEvents = events.filter(event =>
-    //   event.participants.some(team => Array.isArray(team) && team.includes(decoded.userId))
-    // );
+    const userEvents = events.filter(event =>
+        event.participants.includes(decoded.userId)
+      );
 
-    // if (userEvents.length > 0) {
-    //   return NextResponse.json({ success: true, events: userEvents });
-    // }
-    const userEvents = await Promise.all(
-      events.map(async (event) => {
-        const matchedTeam = event.participants.find(
-          (team) => Array.isArray(team) && team.includes(decoded.userId)
-        );
-    
-        if (matchedTeam) {
-          // Populate user data for the matched team
-          const populatedTeam = await User.find({ _id: { $in: matchedTeam } }).select('profile.name profile.avatarUrl');
-          return { ...event.toObject(), matchedTeam: populatedTeam };
-        }
-        return null;
-      })
-    );
-    
-    // Filter out null values (events without matching teams)
-    const filteredUserEvents = userEvents.filter((event) => event !== null);
-    
-    if (filteredUserEvents.length > 0) {
-      return NextResponse.json({ success: true, events: filteredUserEvents });
+    if (userEvents.length > 0) {
+      return NextResponse.json({ success: true, events: userEvents });
     }
-    
 
     return NextResponse.json({ success: false, message: 'No ongoing events found for the user' });
 
